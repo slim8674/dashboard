cb740645e69fef6cf30bc055afd6b4c0ceaa82a0
MarcoCoppola135 <marcocoppola135@gmail.com>
2024-07-09 15:36:36 +0200

    Update status.js

--- status.js
+++ status.js
@@ -1,17 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('/status')
-        .then(response => response.json())
-        .then(statuses => {
-            statuses.forEach(app => {
-                const statusElement = document.getElementById(app.id);
-                if (app.status === 'online') {
-                    statusElement.classList.add('online');
-                } else {
-                    statusElement.classList.add('offline');
-                }
+    function updateStatuses() {
+        fetch('/status')
+            .then(response => response.json())
+            .then(statuses => {
+                statuses.forEach(app => {
+                    const statusElement = document.getElementById(app.id);
+                    if (app.status === 'online') {
+                        statusElement.classList.remove('offline');
+                        statusElement.classList.add('online');
+                    } else {
+                        statusElement.classList.remove('online');
+                        statusElement.classList.add('offline');
+                    }
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching status:', error);
             });
-        })
-        .catch(error => {
-            console.error('Error fetching status:', error);
-        });
+    }
+
+    // Initial status update
+    updateStatuses();
+
+    // Update statuses every 5 seconds
+    setInterval(updateStatuses, 5000);
 });
